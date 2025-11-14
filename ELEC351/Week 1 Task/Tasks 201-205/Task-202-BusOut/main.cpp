e9854378b28c2e627b03c56554fdd6463b84ef0f
milya8 <milya.mohdasyraf@students.plymouth.ac.uk>
2025-11-14 14:56:51 +0000

    o

--- ELEC351/Week 1 Task/Tasks 201-205/Task-202-BusOut/main.cpp
+++ ELEC351/Week 1 Task/Tasks 201-205/Task-202-BusOut/main.cpp
@@ -4,20 +4,38 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
+#define EXTRA1_PIN PB_0
+#define EXTRA2_PIN PB_7
+#define EXTRA3_PIN PB_14
 
 // Objects
 // DigitalOut grn(TRAF_GRN1_PIN);
 // DigitalOut yel(TRAF_YEL1_PIN);
 // DigitalOut red(TRAF_RED1_PIN,1);
-BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
+BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN, PB_0, PB_7, PB_14);
 
 int main()
 {
+    // while (true) {
+    //     leds = 0;   //Binary 000
+    //     wait_us(500000);
+    //     leds = 4;   //Binary 111
+    //     wait_us(500000);    
+    // }
+
+    // //for-loop
+    // while (true) {
+    //     for (int i = 0; i < 8; i++) {
+    //         leds = i;
+    //         wait_us(500000);
+    //     }   
+    // }
+
+    leds = 0b000000;  // Start with all OFF
+
     while (true) {
-        leds = 0;   //Binary 000
+        leds = ~leds;  // Toggle all bits (invert ON/OFF state)
         wait_us(500000);
-        leds = 7;   //Binary 111
-        wait_us(500000);    
     }
 }
 
