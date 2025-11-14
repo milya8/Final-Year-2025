bdfaeb3d66c18c2ca33c230f6f189f0aa96391c2
milya8 <milya.mohdasyraf@students.plymouth.ac.uk>
2025-11-14 16:09:42 +0000

    .

--- ELEC351/Week 1 Task/Tasks 201-205/TASK-203-PortOut/main.cpp
+++ ELEC351/Week 1 Task/Tasks 201-205/TASK-203-PortOut/main.cpp
@@ -4,19 +4,33 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+
+#define LEDMASK_C 0b0000000001001000 
+#define LEDMASK_B 0b0100000010000001   // PB_0, PB_7, PB_14
+
 // Objects
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
-PortOut leds(PortC, LEDMASK);
+PortOut leds(PortC, LEDMASK_C);
+PortOut board_leds(PortB, LEDMASK_B);
 
 int main()
 {
+    // while (true) {
+    //     leds = 0;   //Binary 000
+    //     board_leds = 0;
+    //     wait_us(500000);
+        
+    //     leds = 0xFF;   //Binary 11111111
+    //      board_leds = 0xFFFF;
+    //     wait_us(500000);    
+    // }
+
     while (true) {
-        leds = 0;   //Binary 000
+        leds = leds ^ LEDMASK_C;   // toggle traffic LEDs
+        board_leds = board_leds ^ LEDMASK_B;  // toggle onboard LEDs
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
     }
+
 }
 
 
